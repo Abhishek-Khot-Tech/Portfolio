d9cb0b90a1d245ac424ccc8d49c4291626355b3b
Abhishek Khot <128317286+Abhishek-Khot@users.noreply.github.com>
2025-04-25 22:42:04 +0530

    removed the experience section

--- src/data/experience.jsx
+++ src/data/experience.jsx
@@ -1,29 +1,29 @@
-export const experienceData = [
-  {
-    id: 3,
-    title: "SWE Intern at BlueStock Fintech",
-    period: "Jan 2025 - Feb 2025",
-    workMode: "Remote",
-    location: "Pune , Maharashtra",
-    description: `Worked on software development projects to build scalable and efficient applications. Focused on data structures, code optimization, and collaborating effectively through Git and GitHub. Actively participated in code reviews, debugging, and performance tuning to ensure high-quality and maintainable code.`,
-  },
-  {
-    id: 2,
-    title: "Full Stack Developer at EasyBytes",
-    period: "Nov 2024 - Dec 2025",
-    workMode: "Remote",
-    location: "Meerut , Uttar Pradesh",
-    description: `Worked as a full-stack developer, focusing on backend development with Node.js and React.js for an
-  website. Developed RESTful APIs, secure authentication with JWT/OAuth, database schemas, and
-  booking system features.`,
-  },
-  {
-    id: 1,
-    title: "Cloud Computing Intern at Softmask",
-    period: "Dec 2023 - Jan 2024",
-    workMode: "Offline",
-    location: "Belgaum , Karnataka",
-    description: `Gained practical experience in cloud computing technologies, including AWS and Digital Ocean .Created and managed EC2 instances on AWS and set up Droplets on Digital Ocean for hosting
-applications. Received recognition for professional conduct and a dedicated approach to tasks.`,
-  },
-];
+// export const experienceData = [
+//   {
+//     id: 3,
+//     title: "SWE Intern at BlueStock Fintech",
+//     period: "Jan 2025 - Feb 2025",
+//     workMode: "Remote",
+//     location: "Pune , Maharashtra",
+//     description: `Worked on software development projects to build scalable and efficient applications. Focused on data structures, code optimization, and collaborating effectively through Git and GitHub. Actively participated in code reviews, debugging, and performance tuning to ensure high-quality and maintainable code.`,
+//   },
+//   {
+//     id: 2,
+//     title: "Full Stack Developer at EasyBytes",
+//     period: "Nov 2024 - Dec 2025",
+//     workMode: "Remote",
+//     location: "Meerut , Uttar Pradesh",
+//     description: `Worked as a full-stack developer, focusing on backend development with Node.js and React.js for an
+//   website. Developed RESTful APIs, secure authentication with JWT/OAuth, database schemas, and
+//   booking system features.`,
+//   },
+//   {
+//     id: 1,
+//     title: "Cloud Computing Intern at Softmask",
+//     period: "Dec 2023 - Jan 2024",
+//     workMode: "Offline",
+//     location: "Belgaum , Karnataka",
+//     description: `Gained practical experience in cloud computing technologies, including AWS and Digital Ocean .Created and managed EC2 instances on AWS and set up Droplets on Digital Ocean for hosting
+// applications. Received recognition for professional conduct and a dedicated approach to tasks.`,
+//   },
+// ];
